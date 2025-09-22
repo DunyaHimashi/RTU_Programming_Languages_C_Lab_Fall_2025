b6c7e613084d3e27ae286341a9681ed2b63bc86c
DunyaHimashi <dunya-himashi.thilakarathne@edu.rtu.lv>
2025-09-22 12:07:19 +0300

    Add files via upload
    this is lab2.3

---
+++ src/lab2_3.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+
+/*
+    Task:
+    Write a function `int is_prime(int n)` that returns 1 if n is prime,
+    0 otherwise.
+
+    In main():
+      - Ask user for an integer n (>= 2)
+      - If invalid, print an error
+      - Otherwise, print all prime numbers up to n
+*/
+
+int is_prime(int n) {
+    if (n < 2) return 0;
+    for (int i =2; i<=n/2; i++){
+        if (n%i==0){
+            return 0;
+        }
+    }
+    return 1; // placeholder
+}
+
+int main(void) {
+    int n;
+
+    printf("Enter an integer n (>= 2): ");
+    scanf("%d", &n);
+
+    if (n<2) {
+        printf("Error: n should be greater than 2 \n");
+    } else {
+        printf ("Prime numbers upto %d/n", n);
+        for (int i=2; i<=n; i++){
+            if (is_prime(i)) {
+                printf("%d",i);
+            }
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
